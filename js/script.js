b6065ce7f94073a4b77dc3dad402f2ec3c5825ae
Lee <espiritu.lee@gmail.com>
2023-07-22 13:17:41 +1000

    Home section updates

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ console.log("Remove menu profile is working!")
 
 
 new Typewriter('#typewriter', {
-  strings: ['Biplov Jha', 'Web-developer', 'Electrical-Engineer', 'e-Tutor', 'Author', ''],
+  strings: ['Lee Espiritu', 'Web-developer', 'Software-Developer', 'Project Manager', 'Facilitator', ''],
   autoStart: true,
   loop: true,
   cursor: "|"
